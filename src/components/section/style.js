5c965381a01a2a5cfc2d36a2c7a94d5919d6dd74
DaniyalHusssain <danhussain29@gmail.com>
2024-03-12 14:33:33 +0500

    remove errors from section

--- src/components/section/style.js
+++ src/components/section/style.js
@@ -1,5 +1,4 @@
 import { makeStyles } from "@mui/styles";
-import imageone from "../../Assets/images/backgroundimage.png";
 export const useStyles = makeStyles((theme) => ({
   main: {
     backgroundColor: "#150C30",
